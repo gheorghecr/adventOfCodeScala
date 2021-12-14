582c8a01387d496ef028923ccf2d4f76382dba9c
Gheorghe Craciun <gheorghecr1997@gmail.com>
2021-12-14 21:56:54 +0000

    FInished day4

--- src/main/scala/Day4/Day4.scala
+++ src/main/scala/Day4/Day4.scala
@@ -82,11 +82,11 @@ object Day4 extends App {
         def verifyWinnerColumnHelper(individualBoard: Array[Array[String]], columnIndex: Int): Boolean = {
             if (columnIndex == individualBoard(0).length) false
             else {
-                var valuesPerColumn: List[String] = Nil
+                var valuesPerColumn: Seq[String] = Seq()
                 individualBoard.map{row =>
-                    valuesPerColumn.appended(row(columnIndex))
+                    valuesPerColumn = valuesPerColumn ++ Seq(row(columnIndex).toString())
                 }
-                val numberOfXes = valuesPerColumn.filter(_.toString == "X").length
+                val numberOfXes =  valuesPerColumn.filter(_.toString == "X").length
                 if (numberOfXes == individualBoard.length) true
                 else {
                     verifyWinnerColumnHelper(individualBoard, columnIndex + 1)
@@ -111,13 +111,42 @@ object Day4 extends App {
         }
     }
 
+
+    def playBingoLastBoardPart2(boards: Array[Array[Array[String]]], draws: List[String], winnerBoardsIndexList: List[Int], previousDraw: String): Int = {
+        if (winnerBoardsIndexList.length == boards.length || draws.isEmpty) {
+            return countPoints(boards(winnerBoardsIndexList.last), 0, 0, 0).toInt * previousDraw.toInt
+        }
+        else if (draws.isEmpty) 0
+        else {
+            boards.map { board =>
+                if (!winnerBoardsIndexList.contains(boards.indexOf(board))) {
+                    markDownDrawNumber(board, draws.head, 0, 0)
+                }
+            }
+            boards.map { board =>
+                val result = verifyWinner(board)
+                if (verifyWinner(board) && !winnerBoardsIndexList.contains(boards.indexOf(board)) ) {
+                    var newWinnerBoardsIndexList = winnerBoardsIndexList.appended(boards.indexOf(board)).distinct
+                    return playBingoLastBoardPart2(boards, draws.tail, newWinnerBoardsIndexList, draws.head) 
+                }
+            }
+            playBingoLastBoardPart2(boards, draws.tail, winnerBoardsIndexList, draws.head) 
+        }
+    }
+
     val (input: String, board: Array[Array[Array[String]]]) = readInput("src/main/scala/Day4/input.txt")
     val (inputExample: String, boardExample: Array[Array[Array[String]]]) = readInput("src/main/scala/Day4/input_example.txt")
 
-    val points = playBingoPart1(boardExample, inputExample.split(",").toList)
-    println(points + " POINTS")
+    // val points = playBingoPart1(boardExample, inputExample.split(",").toList)
+    // println(points + " POINTS")
 
-    val points2 = playBingoPart1(board, input.split(",").toList)
-    println(points2 + " POINTS2")
+    // val points2 = playBingoPart1(board, input.split(",").toList)
+    // println(points2 + " POINTS2")
+
+    val pointsPart2 = playBingoLastBoardPart2(boardExample, inputExample.split(",").toList, List(), "0")
+    println(pointsPart2 + " POINTS - PART 2")
+
+    // val points2Part2 = playBingoLastBoardPart2(board, input.split(",").toList, List(), "0")
+    // println(points2Part2 + " POINTS2 - PART 2")
 
 }
